0c565447dd39476f838e098e591af926513cb6e5
Naisarg C <AMDNAV@HC1439.amd.navtech.corp>
2023-01-19 21:34:16 +0530

    First commit:Created the repo and setup basic server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const port = 8000;
+
+const app = express();
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(`Error: ${err}`);
+    return;
+  }
+
+  console.log(`Server running successfully on port ${port}`);
+});
